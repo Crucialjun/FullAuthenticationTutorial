ae9f3fe67e6522cd92584e96e5c66c598762f374
Crucial <nich.otieno@gmail.com>
2021-05-21 06:39:50 +0300

    View Binding

--- app/src/main/java/com/crucialtech/fullauthenticationtutorial/SignUpFragment.kt
+++ app/src/main/java/com/crucialtech/fullauthenticationtutorial/SignUpFragment.kt
@@ -5,15 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.crucialtech.fullauthenticationtutorial.databinding.FragmentSignUpBinding
 
 class SignUpFragment : Fragment() {
+
+    private var _binding : FragmentSignUpBinding? = null
+
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        _binding = FragmentSignUpBinding.inflate(inflater,container,false)
+        val view = binding.root
+        return view
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 
